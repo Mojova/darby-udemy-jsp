3eeafe903383214676f839156ce70a54f4806ac4
Kaisanlahti <j.kaisanlahti@accenture.com>
2021-06-07 09:55:12 +0300

    Initial commit

---
+++ src/main/java/fi/hovukas/udemy_jsp/HelloApplication.java
@@ -0,0 +1,9 @@
+package fi.hovukas.udemy_jsp;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
